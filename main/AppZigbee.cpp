ca5fe4e58fa0fa05793144746b9f596e90999c3b
BDeliers <bdeliers@bdeliers.com>
2026-01-02 16:20:12 +0000

    Implemented sensor cluster

--- main/AppZigbee.cpp
+++ main/AppZigbee.cpp
@@ -4,19 +4,20 @@
 #include "stdio.h"
 #include "string.h"
 
-#include "esp_zigbee_core.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 #include "esp_check.h"
 #include "esp_log.h"
+
 #include "nvs_flash.h"
+
+#include "esp_zigbee_core.h"
 #include "ha/esp_zigbee_ha_standard.h"
 
 static const char *TAG = "AppZigbee";
 
 // --- LOCAL TYPEDEFS ---
-
-// Mirror of the ZB attributes
 typedef struct {
     uint8_t current_position_lift_percentage;
     uint8_t installed_open_limit_lift;
@@ -25,6 +26,20 @@ typedef struct {
     uint8_t installed_open_limit_tilt;
     uint8_t installed_closed_limit_tilt;
 } zb_window_covering_attrs_t;
+
+typedef struct {
+    int16_t 	measure_value;
+    int16_t 	min_measure_value;
+    int16_t 	max_measure_value;
+    uint16_t    tolerance;
+} zb_temperature_measurement_attrs_t;
+
+typedef struct {
+    uint16_t measure_value;
+    uint16_t min_measure_value;
+    uint16_t max_measure_value;
+    uint16_t tolerance;
+} zb_rel_humidity_measurement_attrs_t;
 
 // --- LOCAL VARIABLES  ---
 static zb_window_covering_attrs_t window_covering_ctx = {
@@ -37,7 +52,28 @@ static zb_window_covering_attrs_t window_covering_ctx = {
     .installed_closed_limit_tilt      = 0,
 };
 
-static constexpr uint8_t WINDOW_COVERING_ENDPOINT = 1;
+// Values in °C*100
+static zb_temperature_measurement_attrs_t temperature_measurement_ctx = {
+    .measure_value      = 0,
+    .min_measure_value  = -4000,
+    .max_measure_value  = 12500,
+    .tolerance          = 40,
+};
+
+// Values in %*100
+static zb_rel_humidity_measurement_attrs_t humidity_measurement_ctx = {
+    .measure_value      = 0,
+    .min_measure_value  = 0,
+    .max_measure_value  = 8000,
+    .tolerance          = 300,
+};
+
+
+static constexpr uint8_t WINDOW_COVERING_ENDPOINT   = 1;
+static constexpr uint8_t SENSOR_ENDPOINT            = 10;
+
+static char* manufacturer_id  = "\x07""Balth.D";
+static char* device_id        = "\x15""ZB_Things covering v2";
 
 // --- STATIC FUNCTIONS DECLARATION ---
 static void ZigbeeStartTopLevelCommissionningHandler(uint8_t mode_mask);
@@ -182,8 +218,9 @@ static void ZigbeeTask(void *params)
     // Initialise low-level Zigbee
     esp_zb_init(&zb_nwk_cfg);
 
-    // Configure the endpoint
-    esp_zb_window_covering_cfg_t window_covering_cfg        = ESP_ZB_DEFAULT_WINDOW_COVERING_CONFIG();
+    // -- Configure and create the endpoints
+    // Window covering endpoint (first EP with all settings too)
+    esp_zb_window_covering_cfg_t    window_covering_cfg     = ESP_ZB_DEFAULT_WINDOW_COVERING_CONFIG();
     // Basic config amend: power source
     window_covering_cfg.basic_cfg.power_source              = ESP_ZB_ZCL_BASIC_POWER_SOURCE_DC_SOURCE;
     // Set window covering parameters
@@ -191,34 +228,75 @@ static void ZigbeeTask(void *params)
     window_covering_cfg.window_cfg.covering_type            = ESP_ZB_ZCL_ATTR_WINDOW_COVERING_TYPE_ROLLERSHADE_EXTERIOR;
     window_covering_cfg.window_cfg.covering_status          = ESP_ZB_ZCL_ATTR_WINDOW_COVERING_CONFIG_OPERATIONAL | ESP_ZB_ZCL_ATTR_WINDOW_COVERING_CONFIG_ONLINE;
 
-    esp_zb_ep_list_t *esp_zb_window_covering_ep  = esp_zb_window_covering_ep_create(WINDOW_COVERING_ENDPOINT, &window_covering_cfg);
+    esp_zb_ep_list_t* ep_list = esp_zb_ep_list_create();
 
-    esp_zb_cluster_list_t   *cluster_list       = NULL;
-    esp_zb_attribute_list_t *basic_cluster      = NULL;
-    esp_zb_attribute_list_t *covering_cluster   = NULL;
+    esp_zb_endpoint_config_t window_covering_endpoint_config = {
+        .endpoint               = WINDOW_COVERING_ENDPOINT,
+        .app_profile_id         = ESP_ZB_AF_HA_PROFILE_ID,
+        .app_device_id          = ESP_ZB_HA_WINDOW_COVERING_DEVICE_ID,
+        .app_device_version     = 0,
+    };
 
-    char* manufacturer_id  = "\x07""Balth.D";
-    char* device_id        = "\x15""ZB_Things covering v2";
+    esp_zb_ep_list_add_ep(ep_list, esp_zb_window_covering_clusters_create(&window_covering_cfg), window_covering_endpoint_config);
 
-    cluster_list = esp_zb_ep_list_get_ep(esp_zb_window_covering_ep, WINDOW_COVERING_ENDPOINT);
-    basic_cluster = esp_zb_cluster_list_get_cluster(cluster_list, ESP_ZB_ZCL_CLUSTER_ID_BASIC, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    // Sensor endpoint (fully custom EP)
+    esp_zb_endpoint_config_t sensor_endpoint_config = {
+        .endpoint               = SENSOR_ENDPOINT,
+        .app_profile_id         = ESP_ZB_AF_HA_PROFILE_ID,
+        .app_device_id          = ESP_ZB_HA_TEMPERATURE_SENSOR_DEVICE_ID,
+        .app_device_version     = 0,
+    };
+
+    esp_zb_cluster_list_t *sensor_ep_cluster_list = esp_zb_zcl_cluster_list_create();
+
+    // Add temperature cluster to sensor endpoint
+    esp_zb_temperature_meas_cluster_cfg_t temperature_meas_cfg  = {
+        .measured_value = 0,
+        .min_value      = temperature_measurement_ctx.min_measure_value,
+        .max_value      = temperature_measurement_ctx.max_measure_value,
+    };
+    esp_zb_attribute_list_t *temperature_sensor_cluster = esp_zb_temperature_meas_cluster_create(&temperature_meas_cfg);
+    ESP_ERROR_CHECK(esp_zb_temperature_meas_cluster_add_attr(temperature_sensor_cluster, ESP_ZB_ZCL_ATTR_TEMP_MEASUREMENT_TOLERANCE_ID, &temperature_measurement_ctx.tolerance));
+    ESP_ERROR_CHECK(esp_zb_cluster_list_add_temperature_meas_cluster(sensor_ep_cluster_list, temperature_sensor_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE));
     
+    // Add RH cluster to sensor endpoint
+    esp_zb_humidity_meas_cluster_cfg_t humidity_meas_cfg = {
+        .measured_value = 0,
+        .min_value = humidity_measurement_ctx.min_measure_value,
+        .max_value = humidity_measurement_ctx.max_measure_value,
+    };
+    esp_zb_attribute_list_t *humidity_sensor_cluster = esp_zb_humidity_meas_cluster_create(&humidity_meas_cfg);
+    ESP_ERROR_CHECK(esp_zb_humidity_meas_cluster_add_attr(humidity_sensor_cluster,    ESP_ZB_ZCL_ATTR_REL_HUMIDITY_TOLERANCE_ID, &humidity_measurement_ctx.tolerance));
+    ESP_ERROR_CHECK(esp_zb_cluster_list_add_humidity_meas_cluster(sensor_ep_cluster_list, humidity_sensor_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE));
+
+    // Add the EP
+    esp_zb_ep_list_add_ep(ep_list, sensor_ep_cluster_list, sensor_endpoint_config);    
+
+    // List of clusters on corresponding endpoint
+    esp_zb_cluster_list_t   *cluster_list_covering_ep   = NULL;
+    // Cluster on the endpoints
+    esp_zb_attribute_list_t *basic_cluster              = NULL;
+    esp_zb_attribute_list_t *covering_cluster           = NULL;
+
+    cluster_list_covering_ep = esp_zb_ep_list_get_ep(ep_list, WINDOW_COVERING_ENDPOINT);
+
     // Set device manufacturer and name
+    basic_cluster = esp_zb_cluster_list_get_cluster(cluster_list_covering_ep, ESP_ZB_ZCL_CLUSTER_ID_BASIC, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
     ESP_ERROR_CHECK(esp_zb_basic_cluster_add_attr(basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MANUFACTURER_NAME_ID, static_cast<void*>(manufacturer_id)));
     ESP_ERROR_CHECK(esp_zb_basic_cluster_add_attr(basic_cluster, ESP_ZB_ZCL_ATTR_BASIC_MODEL_IDENTIFIER_ID,  static_cast<void*>(device_id)));
     
     // Register usecase clusters
-    covering_cluster = esp_zb_cluster_list_get_cluster(cluster_list, ESP_ZB_ZCL_CLUSTER_ID_WINDOW_COVERING, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
+    covering_cluster = esp_zb_cluster_list_get_cluster(cluster_list_covering_ep, ESP_ZB_ZCL_CLUSTER_ID_WINDOW_COVERING, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_CURRENT_POSITION_LIFT_PERCENTAGE_ID,  &window_covering_ctx.current_position_lift_percentage));
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_INSTALLED_OPEN_LIMIT_LIFT_ID,         &window_covering_ctx.installed_open_limit_lift));
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_INSTALLED_CLOSED_LIMIT_LIFT_ID,       &window_covering_ctx.installed_closed_limit_lift));
-
+    
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_CURRENT_POSITION_TILT_PERCENTAGE_ID,  &window_covering_ctx.current_position_tilt_percentage));
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_INSTALLED_OPEN_LIMIT_TILT_ID,         &window_covering_ctx.installed_open_limit_tilt));
     ESP_ERROR_CHECK(esp_zb_window_covering_cluster_add_attr(covering_cluster, ESP_ZB_ZCL_ATTR_WINDOW_COVERING_INSTALLED_CLOSED_LIMIT_TILT_ID,       &window_covering_ctx.installed_closed_limit_tilt));
-
-    // Register endpoint
-    esp_zb_device_register(esp_zb_window_covering_ep);
+    
+    // Register endpoints
+    esp_zb_device_register(ep_list);
     
     // Register Zigbee main handler
     esp_zb_core_action_handler_register(ZigbeeHandler);
